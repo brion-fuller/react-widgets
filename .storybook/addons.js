956fd0a0ccaeec10eccf4020e2613ff554d63756
Brion Fuller <Brion Fuller>
2017-03-25 20:21:43 -0500

    update to storybook

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -2,5 +2,3 @@
 import '@kadira/storybook/addons';
 //  To add the knobs addon
 //import '@kadira/storybook-addon-knobs/register'
-
-// import 'storybook-addon-specifications/register';
--- src/Checkbox/story.jsx
+++ src/Checkbox/story.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
-import { specs } from 'storybook-addon-specifications';
 
 import Checkbox from './index';
 import CheckBoxWrapper from './wrapper';
-import spec from './spec';
 
 storiesOf('<Checkbox />', module)
   .add('with label', () => (<Checkbox id={'checkbox'} label={'Checkbox'} />))
   .add('with label checked', () => (<Checkbox id={'checkbox'} label={'Checkbox'} checked />))// $('#checkbox').focus()
   .add('with focus', () => {
-    specs(() => spec);
     setTimeout(() => document.getElementById('checkbox').focus(), 750);
     return (<Checkbox id={'checkbox'} label={'Checkbox'} checked />);
   })
